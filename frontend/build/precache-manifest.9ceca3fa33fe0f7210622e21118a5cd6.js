0fbca6434cff063e61839d23524586b41a0d8558
Gagan Diwan <azurepro@maas-MacBook-Pro.local>
2022-12-05 22:48:56 +0200

    some hack again

---
+++ frontend/build/precache-manifest.9ceca3fa33fe0f7210622e21118a5cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a877d963b0fbde5c80567f720c011b",
+    "url": "/bonus-front/index.html"
+  },
+  {
+    "revision": "aa9a19ffdf9b7c2ed37a",
+    "url": "/bonus-front/static/css/main.bb1cd6a4.chunk.css"
+  },
+  {
+    "revision": "313ccda13af906be5d2b",
+    "url": "/bonus-front/static/js/2.493e2401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bonus-front/static/js/2.493e2401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9a19ffdf9b7c2ed37a",
+    "url": "/bonus-front/static/js/main.f00f6663.chunk.js"
+  },
+  {
+    "revision": "083a62bb4f12ddac6583",
+    "url": "/bonus-front/static/js/runtime-main.188109f3.js"
+  }
+]);
